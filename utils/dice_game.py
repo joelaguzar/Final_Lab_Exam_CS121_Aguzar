606ce513103e2bacd8341975e8ea3b757ea1dbdd
joelaguzar <23-00562@g.batstate-u.edu.ph>
2024-05-10 09:58:02 +0800

    added load_scores method

--- utils/dice_game.py
+++ utils/dice_game.py
@@ -13,4 +13,21 @@ class DiceGame:
         self.load_scores()
         
     def load_scores(self):
+        try:
+            with open("data/rankings.txt", "r") as file:
+                for line in file:
+                    username, game_id, points, wins = line.strip().split("|")
+                    score = Score(username, (game_id))
+                    score.points = int(points)
+                    score.wins = int(wins)
+                    self.scores.append(score)
+        except FileNotFoundError:
+            return None
+        except ValueError:
+            print("Error: Invalid data in ranking file.")
+            return None
         
+    def save_scores(self):
+        with open("data/rankings.txt", "w") as file:
+            for score in self.scores:
+                file.write(f"{score.username}|{score.game_id}|{score.points}|{score.wins}\n")
