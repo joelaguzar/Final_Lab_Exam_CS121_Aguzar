f33bdd62c674cfd1e781ef432e9d78c671c895d7
joelaguzar <23-00562@g.batstate-u.edu.ph>
2024-05-10 09:24:54 +0800

    created class UserManager

---
+++ utils/user_manager.py
@@ -0,0 +1,11 @@
+from .user import User
+import os
+
+class UserManager:
+    def __init__(self):
+        self.users = {}
+        self.load_users()
+        
+        def load_users(self):
+            pass
+            
