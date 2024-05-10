cd7c4b16d25a6f23b0cefe264b2a55a1442eaa71
joelaguzar <23-00562@g.batstate-u.edu.ph>
2024-05-10 10:25:00 +0800

    added play_game method

--- utils/dice_game.py
+++ utils/dice_game.py
@@ -32,4 +32,53 @@ class DiceGame:
             for score in self.scores:
                 file.write(f"{score.username}|{score.game_id}|{score.points}|{score.wins}\n")
     
-    def play
+    def play_game(self):
+        if not self.current_user:
+            print("\nYou need to log in to play the game.")
+            return
+        player_name = self.current_user.username
+
+        now = datetime.datetime.now()
+        game_id = now.strftime("%d %b %Y %I:%M %p") 
+        current_score = Score(self.current_user.username, game_id)
+        stage = 1
+
+        while True:
+            print(f"\n--- STAGE {stage} ---")
+            player_wins = 0
+            computer_wins = 0
+
+            rounds = 3
+            while rounds > 0:
+                player_roll = random.randint(1, 6)
+                computer_roll = random.randint(1, 6)
+                print(f"\n{player_name} rolled: {player_roll}")
+                print(f"Computer rolled: {computer_roll}")
+
+                if player_roll > computer_roll:
+                    print(f"{player_name} win this round!")
+                    player_wins += 1
+                    current_score.points += 1
+                elif computer_roll > player_roll:
+                    print("Computer wins this round!")
+                    computer_wins += 1
+                else:
+                    print("It's a tie! Roll again.")
+                rounds -= 1
+
+            if player_wins >= 2:
+                print(f"\n{player_name} won stage {stage}!")
+                current_score.points += 3
+                current_score.wins += 1
+                choice = input("\nEnter any character to continue to the next stage, or 0 to stop playing: ")
+                if choice == '0':
+                    break
+                stage += 1
+            else:
+                print("Game over. You didn't win any stage.")
+                break
+
+        self.scores.append(current_score)
+        self.save_scores()
+        print(f"\nTotal Points: {current_score.points}")
+        print(f"Stages Won: {current_score.wins}")
