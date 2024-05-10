08b2fc5ec626da5f458e13128fad8d56cd4bf741
joelaguzar <23-00562@g.batstate-u.edu.ph>
2024-05-10 09:32:59 +0800

    added load_user method

--- utils/user_manager.py
+++ utils/user_manager.py
@@ -6,6 +6,16 @@ class UserManager:
         self.users = {}
         self.load_users()
         
-        def load_users(self):
-            pass
+    def load_users(self):
+        if not os.path.exist("data"):
+            os.maskedir("data")
+        try:
+            with open("data/users.txt" , "r") as file:
+                for lime in file:
+                    username, password = line.strip().split("|")
+                    self.users[username] = password
+        except FileNotFoundError:
+            return None
+        except ValueError:
+            return None
             
